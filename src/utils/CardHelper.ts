dc1cf939223080c4041e6e4a6d84b3ee46ea9ade
Harmiox <harmiox@gmail.com>
2018-04-04 19:54:21 -0400

    Fixed referencing

--- src/utils/CardHelper.ts
+++ src/utils/CardHelper.ts
@@ -135,14 +135,14 @@ export module CardHelper {
    * Returns all card details sorted by their name.
    */
   export function getAllCardsSortedByName():  Map<string, ICardDetails> {
-    return this.cardsByName;
+    return cardsByName;
   }
 
   /**
    * Returns all card details sorted by their id.
    */
   export function getAllCardsSortedById():  Map<number, ICardDetails> {
-    return this.cardsById;
+    return cardsById;
   }
 
   /**
